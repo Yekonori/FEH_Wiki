a8f12a46020aba74dd60bd5778192c712ab803ee
Yannis <yannis1403@hotmail.fr>
2017-05-12 01:58:47 +0200

    Ajout des Heros Bonus d'Arene

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
 <?php
-  #1. Connexion à la base de donnée
-  include("ConnexionFE.php");
+    #1. Connexion à la base de donnée
+    include("ConnexionFE.php");
 
-  $heros = "SELECT * FROM `héros` ORDER BY `ID :`";
+    $heros = "SELECT `Nom :` FROM `héros` ORDER BY `ID :`";
+    $mesHeros = mysqli_query($link, $heros);
 
-  $mesHeros = mysqli_query($link, $heros);
+    $LastHeros = "SELECT `Nom :`FROM `héros` ORDER BY `ID :` DESC LIMIT 0, 4";
+    $NvHeros = mysqli_query($link, $LastHeros);
 
-  if (mysqli_connect_errno($link)) {
-    die("Erreur de connexion : ".mysqli_connect_error($link));
-  }
+    $annonces = "SELECT * FROM `annonces` ORDER BY `ID :` DESC LIMIT 0, 3";
+    $dernieresAnnonces = mysqli_query($link, $annonces);
+
+    $persoArene = "SELECT `Nom :` FROM `bonus_arene` ORDER BY `ID :`";
+    $bonusArene = mysqli_query($link, $persoArene);
+
+    if (mysqli_connect_errno($link)) {
+        die("Erreur de connexion : ".mysqli_connect_error($link));
+    }
 ?>
 
 <!DOCTYPE html>
@@ -63,45 +71,42 @@
     							<a href="#"><img src="#" title="#" alt="#"/>---LIEN RAPIDE---</a>
     						</div> <!-- ./end div.col-md-12 -->
     					</div> <!-- ./end div.row 3-->
-    					<div class="col-md-7 text-center">
+                        <div class="col-md-7 text-center">
     						<h1>Nouveaux Héros :</h1>
     						<div class="row 4">
     						<!-- ******************************** Derniers Héros ******************************** -->
 	    						<div class="col-md-12">
-	    							<?php
-						                $LastHeros = "SELECT `Nom :`FROM `héros` ORDER BY `ID :` DESC LIMIT 0, 4";
-
-						                $NvHeros = mysqli_query($link, $LastHeros);
-						                $i = 0;
-						                while ($data = mysqli_fetch_assoc($NvHeros)) {
-						                  echo '<div class="text-center" style="display:inline-block; width:24%;">
-						                          <p>
-						                            <img src="images/icons/Icon_'.$data['Nom :'].'.png" style="border:1.5px solid rgba(175, 174, 174, 0.85); width:40%"/>
-						                            <br/>
-						                            <a href="#">'.$data['Nom :'].'</a>
-						                          </p>
-						                        </div>';
-						                  $i++;
-						                }
-						            ?>
+                                    <?php
+					                   $i = 0;
+					                   while ($data = mysqli_fetch_assoc($NvHeros)) {
+					                       echo '<div class="text-center" style="display:inline-block; width:24%;">
+				                                    <p>
+					                                   <img src="images/icons/Icon_'.$data['Nom :'].'.png" style="border:1.5px solid rgba(175, 174, 174, 0.85); width:40%"/>
+					                                   <br/>
+					                                   <a href="#">'.$data['Nom :'].'</a>
+				                                    </p>
+                                                </div>';
+                                            $i++;
+                                        }
+                                    ?>
 						            <hr/>
 	    						</div> <!-- ./end div.col-md-12 -->
 	    						<div class="col-md-12">
 	    						<!-- ******************************** Tous les Héros ******************************** -->
 	    							<h2>Les Héros :</h2>
-	    							<?php
-						                $i = 0;
-						                while ($data = mysqli_fetch_assoc($mesHeros)) {
-						                	echo '<div class="text-center" style="display:inline-block; width:8%;">
-						                        	<p>
-						                        		<a href="#">
-						                            	<img src="images/icons/Icon_'.$data['Nom :'].'.png" style="border:1.5px solid rgba(175, 174, 174, 0.85); border-radius:100%; width:80%;"/>
-						                            	</a>
-						                        	</p>
-						                        </div>';
-						                	$i++;
-						            	}
-						            ?>
+                                    <?php
+                                        $i = 0;
+                                        while ($data = mysqli_fetch_assoc($mesHeros)) {
+                                        echo '<div class="text-center" style="display:inline-block; width:8%; margin:5px;">
+                                                <p>
+                                                    <a href="#">
+                                                        <img src="images/icons/Icon_'.$data['Nom :'].'.png" style="border:1.5px solid rgba(175, 174, 174, 0.85); border-radius:100%; width:100%;"/>
+                                                    </a>
+                                                </p>
+                                            </div>';
+                                            $i++;
+                                        }
+                                    ?>
 	    						</div> <!-- ./end div.col-md-12 -->
     						</div> <!-- ./end div.row 4 -->
     					</div> <!-- ./end div.col-md-7 -->
@@ -115,14 +120,16 @@
     									</div> <!-- ./end div.col-md-12	-->
     									<div class="col-md-12 text-left">
     									<!-- Créer une table MàJ&Event -->
-    										<ul>
-    											<li>[8 Mai 2017]</li>
-    											<p>Version 1.3.0 : MàJ de l'arène, Tournois annoncé et Evenement Exp x1,5. </p>
-    											<li>[2 Mai 2017]</li>
-    											<p>Grande Bataille contre Xander</p>
-    											<li>[28 Avril 2017]</li>
-    											<p>Focus : Festival des Héros</p>
-    										</ul>
+    									<ul>
+    									<?php
+							                $i = 0;
+							                while ($data = mysqli_fetch_assoc($dernieresAnnonces)) {
+							                	echo '<li>'.$data['Titre :'].'</li>';
+							                	echo '<p>'.$data['Description :'].'</p>';
+							                	$i++;
+							            	}
+							            ?>
+							            </ul>
     									</div> <!-- ./end div.col-md-12	-->
     								</div> <!-- ./end div.row 6 -->
     							</div> <!-- ./end div.col-md-12 -->
@@ -132,14 +139,16 @@
     										<h5>Héros Bonus d'Arène :</h5>
     									</div> <!-- ./end div.col-md-12 -->
     									<div class="col-md-12">
-    										<img src="#" title="#" alt="#"/>
-    										<img src="#" title="#" alt="#"/>
-    										<img src="#" title="#" alt="#"/>
-    										<img src="#" title="#" alt="#"/>
-    										<img src="#" title="#" alt="#"/>
-    										<img src="#" title="#" alt="#"/>
-    										<img src="#" title="#" alt="#"/>
-    										<img src="#" title="#" alt="#"/>
+                                            <?php
+                                                $i = 0;
+                                                while ($data = mysqli_fetch_assoc($bonusArene)) {
+                                                    echo '<img src="images/icons/Icon_'.$data['Nom :'].'.png" style="border:1.5px solid rgba(175, 174, 174, 0.85); border-radius:100%; width:12%;"/>';
+                                                    if ($i == 4) {
+                                                        echo '<br/>';
+                                                    }
+                                                    $i++;
+                                                }
+                                            ?>
     									</div> <!-- ./end div.col-md-12 -->
     								</div> <!-- ./end div.row 7 -->
     							</div> <!-- ./end div.col-md-12 -->
